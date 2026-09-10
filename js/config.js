feb4c8e4d90c71723e702703c140e9149168f738
kubahaha <kubix.medrek@gmail.com>
2026-09-10 21:37:35 +0200

    IV liga Lbn

--- js/config.js
+++ js/config.js
@@ -3,5 +3,6 @@ export const leagues = [
   { id: '1-liga', name: '1. Liga', wikidataId: 'Q139892278', participants: 18 },
   { id: '2-liga', name: '2. Liga', wikidataId: 'Q139940116', participants: 18 },
   { id: '3-liga-1', name: '3. Liga (grupa 1)', wikidataId: 'Q141407107', participants: 18 },
-  { id: '3-liga-4', name: '3. Liga (grupa 4)', wikidataId: 'Q141414428', participants: 18 }
+  { id: '3-liga-4', name: '3. Liga (grupa 4)', wikidataId: 'Q141414428', participants: 18 },
+  { id: '4-liga-lu', name: '4. Liga (grupa lubelska)', wikidataId: 'Q141416127', participants: 18 }
 ];
