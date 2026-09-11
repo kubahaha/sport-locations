70760d07443ce7aaafd15aa7e1d2e5b1fb9c13c1
kubahaha <kubix.medrek@gmail.com>
2026-09-11 13:26:16 +0200

    nowa liga

--- js/config.js
+++ js/config.js
@@ -5,5 +5,6 @@ export const leagues = [
   { id: '3-liga-1', name: '3. Liga (grupa 1)', wikidataId: 'Q141407107', participants: 18 },
   { id: '3-liga-4', name: '3. Liga (grupa 4)', wikidataId: 'Q141414428', participants: 18 },
   { id: '4-liga-lu', name: '4. Liga (grupa lubelska)', wikidataId: 'Q141416127', participants: 16 },
-  { id: 'okregowka-lu', name: 'Klasa okręgowa (grupa lubelska)', wikidataId: 'Q141417681', participants: 14 }
+  { id: 'okregowka-lu', name: 'Klasa okręgowa (grupa lubelska)', wikidataId: 'Q141417681', participants: 14 },
+  { id: 'okregowka-lb', name: 'Klasa okręgowa (grupa bielskopodlaska)', wikidataId: 'Q141431068', participants: 16 }
 ];
